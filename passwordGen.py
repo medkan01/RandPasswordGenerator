ebae3f7157e6537429c2bf618536952fb10c4914
medkan01 <mehdiakniou@gmail.com>
2021-01-13 15:49:47 +0100

    definition of the first functions

--- passwordGen.py
+++ passwordGen.py
@@ -1,2 +1,30 @@
-def randInt(size):
-    return
+import random
+
+def randInt(lb, ub):
+    """
+        Return integer between lb and ub
+
+    Args:
+    
+        lb (int): lower bound
+        
+        ub (int): upper bound
+    """
+    return random.randint(lb, ub)
+
+def generator(lowercase = True, uppercase = False, number = False, symbol = False):
+    """
+        Generate random password with the differents arguments needed
+        
+        Default: Only lowercases are selected
+
+    Args:
+    
+        lowercase ([type]): [description]
+        
+        uppercase ([type]): [description]
+        
+        number ([type]): [description]
+        
+        symbol ([type]): [description]
+    """
