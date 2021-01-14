511475aa31439b3459192f27377989c426572bdf
medkan01 <mehdiakniou@gmail.com>
2021-01-14 04:05:10 +0100

    update view

--- View/view.py
+++ View/view.py
@@ -3,12 +3,12 @@ import PySimpleGUI as sg
 bg = "#161b22"
 
 layout = [
-    [sg.Text("Size", background_color=bg), sg.InputText(size=(10,10))],
+    [sg.Text("Size", background_color=bg), sg.Spin([i for i in range(1,20)], initial_value=8, size=(3,10))],
     
     [sg.HSeparator()],
     
     [sg.Text("Select the character's type you want", background_color=bg)],
-    [sg.CBox("Lowercase", background_color=bg)],
+    [sg.CBox("Lowercase", background_color=bg, default=True, disabled=True)],
     [sg.CBox("Uppercase", background_color=bg)],
     [sg.CBox("Number", background_color=bg)],
     [sg.CBox("Symbol", background_color=bg)],
